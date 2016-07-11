f92fbba29444371dc58a490c78dcfd650ae673cd
eliilek@gmail.com <eliilek@gmail.com>
2016-07-10 23:21:32 -0500

    Playing around

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import os
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
+    times = int(os.environ.get('TIMES', 3))
     r = requests.get('http://httpbin.org/status/418')
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return HttpResponse(('<pre>' + r.text + '</pre>') * times)
 
 
 def db(request):
